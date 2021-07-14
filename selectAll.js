5d57dcf61d68bb625acaf9d2e8cc7ac47af92ff7
Greg <Gdixon5125@gmail.com>
2021-07-14 15:38:37 -0400

    Node and MySQL

---
+++ selectAll.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+
+var DB_NAME = 'nodeDB'
+var sql = 'CREATE DATABASE IF NOT EXISTS ?';
+var inserts = [DB_NAME];
+
+// Create DB Connection
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: " ",
+    port: "4000"
+});
+
+// Connect to MySQL
+con.connect(function(err) {
+    console.log("DB_NAME: " + DB_NAME);
+console.log("SQL: " + sql);
+
+    if (err) throw err;
+    console.log("Connected to the Database");
+    con.query(sql, inserts, function(err, result){
+        if (err) throw err;
+    console.log("Database created");
+    con.database = DB_NAME
+    console.log("Database: " + con.database);
+    });
+});
